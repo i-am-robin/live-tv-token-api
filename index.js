188502f7bfa451df1f913fa9423cdb1fe66af9fd
i-am-robin <developerrobin@gmail.com>
2023-12-02 13:21:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const puppeteer = require("puppeteer");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.get("/token", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--disable-features=site-per-process"],
+    });
+
+    const page = await browser.newPage();
+
+    // Enable request interception
+    await page.setRequestInterception(true);
+
+    let matchingURL = null;
+
+    page.on("request", async (request) => {
+      const url = request.url();
+      console.log(url);
+
+      if (url.includes("http://tv.ebox.live:8080/roarzone/") && !matchingURL) {
+        matchingURL = url;
+        await browser.close();
+      } else {
+        // Continue other requests
+        request.continue();
+      }
+    });
+
+    await page.goto("http://tv.ebox.live/");
+
+    // Close the browser if no matching request is found within a certain time
+    setTimeout(async () => {
+      await browser.close();
+    }, 5000); // Adjust the timeout value as needed
+
+    if (matchingURL) {
+      const match = matchingURL.match(/token=([^&]*)/);
+      const tokenValue = match ? match[1] : null;
+      res.json({ url: matchingURL, token: tokenValue });
+    } else {
+      res.json({ error: "No matching URL found" });
+    }
+  } catch (error) {
+    res.json({ error: "No matching URL found" });
+  }
+});
+
+app.listen("5000", () => console.log("server is runing on port 5000"));
