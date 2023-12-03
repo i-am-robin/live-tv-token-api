906c4803fe2f23dd452be6f84d827ae3fea0f693
i-am-robin <developerrobin@gmail.com>
2023-12-03 14:20:42 +0600

    first commit

--- index.js
+++ index.js
@@ -1,78 +1,22 @@
-const puppeteer = require("puppeteer");
-const express = require("express");
-const cors = require("cors");
-const app = express();
+require("dotenv").config();
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
-app.use(cors());
+const app = require("express")();
 
-app.get("/", (req, res) => {
-  res.send("server is running on port 5000");
-});
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.CHORME_EXICUTEABLE_PATH || (await chromium.executablePath),
+    headless: true,
+  });
 
-app.get("/token", async (req, res) => {
-  const browser = await puppeteer.launch({});
   const page = await browser.newPage();
+  await page.goto("https://pptr.dev/");
+  const title = await page.title();
 
-  const w = await page.goto(
-    "https://www.youtube.com/watch?v=qo_fUjb02ns&t=183s"
-  );
-  console.log(w.headers());
-
-  res.send(w.headers());
+  res.send(title);
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// try {
-//   const browser = await puppeteer.launch({
-//     args: [
-//       "--disable-setuid-sandbox",
-//       "--no-sandbox",
-//       "--single-process",
-//       "--no-zygote",
-//     ],
-//     executablePath:
-//       process.env.NODE_ENV === "production"
-//         ? process.env.PUPPETEER_EXECUTABLE_PATH
-//         : puppeteer.executablePath(),
-//   });
-
-//   const page = await browser.newPage();
-
-//   // Enable request interception
-//   await page.setRequestInterception(true);
-
-//   let matchingURL = null;
-
-//   page.on("request", async (request) => {
-//     const url = request.url();
-//     console.log(url);
-
-//     if (url.includes("http://tv.ebox.live:8080/roarzone/") && !matchingURL) {
-//       matchingURL = url;
-//       console.log(matchingURL);
-//     }
-
-//     // Continue other requests
-//     request.continue();
-//   });
-
-//   await page.goto("http://tv.ebox.live/", { timeout: 60000 });
-
-//   // Close the browser after a certain time
-//   setTimeout(async () => {
-//     await browser.close();
-//   }, 7000); // Adjust the timeout value as needed
-//   if (matchingURL) {
-//     const match = matchingURL.match(/token=([^&]*)/);
-//     const tokenValue = match ? match[1] : null;
-//     console.log(tokenValue);
-//     res.json({ url: matchingURL, token: tokenValue });
-//   } else {
-//     res.json({ error: "No matching URL found" });
-//   }
-// } catch (error) {
-//   console.log(error);
-//   res.json({ error: error });
-// }
+app.listen(5000, () => console.log("server is runing on port 5000..."));
