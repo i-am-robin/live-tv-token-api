e378f65b7e91c18b18a11d40d90b46938288acec
i-am-robin <developerrobin@gmail.com>
2023-12-03 06:08:01 +0600

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/token", async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   try {
     const browser = await puppeteer.launch({
       headless: "new",
